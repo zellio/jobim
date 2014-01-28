44193164f368c7a6c480b1f53773c0e39ff690c0
Zachary Elliott <zach@nyu.edu>
2014-01-27 19:25:58 -0500

    Adding option default tests

--- spec/jobim_settings_spec.rb
+++ spec/jobim_settings_spec.rb
@@ -8,6 +8,31 @@ describe Jobim::Settings do
   end
 
   describe "#options", fakefs: true do
+    subject(:options) { Jobim::Settings.new.options }
+
+    it 'defaults :Daemonize to false' do
+      options[:Daemonize].should be_false
+    end
+
+    it 'defaults :Dir to current working directory' do
+      options[:Dir].should be Dir.pwd
+    end
+
+    it 'defaults :Host to localhost' do
+      options[:Host].should be 'localhost'
+    end
+
+    it 'defaults :Port to 3000' do
+      options[:Port].should be 3000
+    end
+
+    it 'default :Prefix to /' do
+      options[:Prefix].should be '/'
+    end
+
+    it 'defaults :Quiet to false' do
+      options[:Quiet].should be_false
+    end
   end
 
   describe "#load_file" do
