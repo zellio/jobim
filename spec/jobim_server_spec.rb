22ed71742ac9281a56163238f5d29b393dba7584
Zachary Elliott <zach@nyu.edu>
2014-02-02 14:55:02 -0500

    Adding server spec skeleton

--- spec/jobim_server_spec.rb
+++ spec/jobim_server_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
-describe Jobim do
+describe Jobim::Server, fakefs: true do
 
-  describe "::start" do
+  include Rack::Test::Methods
+
+  describe "::start!" do
+  end
+
+  describe 'app' do
+  end
+
+  describe 'server' do
+  end
+
+  describe '#start' do
+  end
+
+  describe '#stop' do
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,5 @@ require 'fakefs/spec_helpers'
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers, fakefs: true
 end
+
+require 'rack/test'
