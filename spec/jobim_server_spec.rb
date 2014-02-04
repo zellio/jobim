87c856810bac996aa7c8052025a8361595322e71
Zachary Elliott <zach@nyu.edu>
2014-02-03 20:38:25 -0500

    Adding server tests

--- spec/jobim_server_spec.rb
+++ spec/jobim_server_spec.rb
@@ -2,21 +2,61 @@ require 'spec_helper'
 
 describe Jobim::Server, fakefs: true do
 
-  include Rack::Test::Methods
+  before(:each) do
+    Dir.mkdir("/web_app")
+    Dir.mkdir("/web_app/trail_test")
+    Dir.mkdir("/web_app/index_test")
 
-  describe "::start!" do
+    File.open("/web_app/index_test/index.html", "w") {|f| f.write(index) }
+    File.open("/web_app/trail_test/foo.html", "w")   {|f| f.write(index) }
+    File.open("/web_app/trail_test/foo", "w") {|f| f.write(index) }
   end
 
-  describe 'app' do
+  let(:index)    { "<!DOCTYPE html>\n<html />\n" }
+  let(:settings) { settings = Jobim::Settings.new }
+
+  describe 'app', rack_test: true do
+    before(:each) { $stdout.stub(:write) }
+
+    let(:server) {
+      settings.options[:Dir] = '/web_app'
+      Jobim::Server.new(settings.options)
+    }
+
+    let(:app) { server.app }
+
+    it 'returns index.html from dirs when it exists' do
+      get '/index_test/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eql index
+    end
+
+    it 'treats bare urls as directories when they are' do
+      get '/trail_test'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include "/trail_test/foo.html"
+    end
+
+    it 'treats bare urls as files when they are' do
+      get '/trail_test/foo'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eql index
+    end
   end
 
   describe 'server' do
-  end
+    let(:server) {
+      settings.options[:Daemonize] = true
+      Jobim::Server.new(settings.options)
+    }
 
-  describe '#start' do
-  end
+    it 'sets server.pid_file if :Daemonize is true' do
+      expect(server.server.pid_file).to eql('jobim.pid')
+    end
 
-  describe '#stop' do
+    it 'sets server.log_file if :Daemonize is true' do
+      expect(server.server.log_file).to eql('jobim.log')
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jobim'
 
 require 'fakefs/spec_helpers'
+require 'rack/test'
 
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers, fakefs: true
+  config.include Rack::Test::Methods, rack_test: true
 end
-
-require 'rack/test'
