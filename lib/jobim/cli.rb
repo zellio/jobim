4244b2f935dd6afa39f2bb1aa1a8ab7f0a67f747
Zachary Elliott <zach@nyu.edu>
2014-02-05 03:04:54 -0500

    Adding documentation to the Jobim::CLI object

--- lib/jobim/cli.rb
+++ lib/jobim/cli.rb
@@ -1,17 +1,32 @@
 require 'optparse'
 
+# Command line interface for the Jobim application. Utilizes optparse to
+# manage user arguments and populates a Jobim::Settings object with those
+# data.
 class Jobim::CLI
 
   attr_reader :parser, :settings, :exit
 
+  # Memoized accessor for the settings object. Lazily initializes a new
+  # Jobim::Settings object when accessed.
+  #
+  # @return [Jobim::Settings]
   def settings
     @settings ||= Jobim::Settings.new
   end
 
+  # Accessor method for the hash contained in the owned Jobim::Settings
+  # object. Directly delegates access to the value returned by #settings
+  #
+  # @return [Hash]
   def options
     settings.options
   end
 
+  # Memoized accessor for the OptionParser object. It is generated only when
+  # called upon. See the readme for information about the command flags.
+  #
+  # @return [OptionParser]
   def parser
     @parser ||= OptionParser.new do |o|
       o.banner = "Usage: jobim [OPTION]... [DIRECTORY]"
@@ -61,11 +76,23 @@ class Jobim::CLI
     end
   end
 
+  # Runs the parse method of the value returned by the #parser method. This is
+  # done in a manner destructive to the passed args Array. If there is a
+  # trailing value after parsing is completed it is treated as the :Dir
+  # option.
+  #
+  # @param [Array<String] args to be paresed
+  # @return [NilClass] sentitinal nil value
   def parse(args)
     parser.parse!(args)
     options[:Dir] = File.expand_path(args[0]) if args.length == 1
+
+    nil
   end
 
+  # Returns the help documentation of the program
+  #
+  # @return [String]
   def help
     parser.to_s
   end
